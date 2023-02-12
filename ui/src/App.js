45ac6214784024ef87edbdbe0195d527798b3398
Julian Schambeck <45296090+julianschambeck@users.noreply.github.com>
2023-02-12 06:28:10 +0100

    basic login flow; add prettier config

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,41 @@
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-        <p>Hello World</p>
-        <a href="http://localhost:8888/login">Log in with Spotify</a>
-    </div>
-  );
+    const initialState = {
+        isLoggedIn: false,
+        accessToken: "",
+        refreshToken: ""
+    };
+    const [userState, setUserState] = useState(initialState);
+    function storeToken() {
+        const url = new URL(
+            window.location.origin + window.location.pathname.replace("/", "?")
+        );
+        const params = new URLSearchParams(url.search);
+        setUserState({
+            isLoggedIn: params.has("access_token"),
+            accessToken: params.get("access_token"),
+            refreshToken: params.get("refresh_token")
+        });
+    }
+    return (
+        <>
+            {userState.isLoggedIn ? (
+                <div>Welcome to home page</div>
+            ) : (
+                <div>
+                    <p>
+                        Press <a href="http://localhost:8888/login">here</a> to
+                        login to Spotify and authorize access to relevant
+                        Spotify data
+                    </p>
+                    <div>
+                        <button onClick={storeToken}>Store token</button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default App;
